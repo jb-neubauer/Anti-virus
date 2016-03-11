b8cd62942f562de1668cc56e5689c3f51cdfe08e
nebulousenhanced <nebulousenhanced@NebulousEnhanced.com>
2016-03-11 15:13:26 -0500

    Configure file to pep8 standard

--- pscanner.py
+++ pscanner.py
@@ -12,15 +12,15 @@ remoteServer = raw_input("Enter the host you would like to scan")
 remoteServerIP = socket.gethostbyname(remoteServer)
 
 if remoteServer ==""
-	print("_"*60)
-	print("Scanning local computer")
-	print("_"*60)
-	remoteServer = "127.0.0.1"
-	remoteServer1 = socket.gethostbyname(remoteServer)
+    print("_"*60)
+    print("Scanning local computer")
+    print("_"*60)
+    remoteServer = "127.0.0.1"
+    remoteServer1 = socket.gethostbyname(remoteServer)
 else 
-	print("_"*60)
-	print("Scanning $d %(remoteServerIP)")
-	print("_"*60)
+    print("_"*60)
+    print("Scanning $d %(remoteServerIP)")
+    print("_"*60)
 
 # Check time when the scan was started
 t1 = datetime.now()
@@ -32,18 +32,18 @@ Using range to find the specific ports with Error handling
 safePorts = [22, 443, 53, 465,  
 
 try:
-	for port in range(1, 1025) 
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.SOCK_DGRAM)
-		result = sock.connect_ex((remoteServerIP, port))
-		if result == 0
-			print("Port{}:	Open".format(port))
-		elif result == 0
-			print("Port{}:	Open".format(port))
-		sock.close
+    for port in range(1, 1025) 
+ 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.SOCK_DGRAM)
+	result = sock.connect_ex((remoteServerIP, port))
+	if result == 0
+            print("Port{}:Open".format(port))
+	elif result == 0
+	    print("Port{}:Open".format(port))
+	sock.close
 
 except Keyboardinterrupt:
-	print("You pressed Ctrl+C")
-	sys.exit()
+    print("You pressed Ctrl+C")
+    sys.exit()
 
 except socket.gaierror:
     print 'Hostname could not be resolved. Exiting'
